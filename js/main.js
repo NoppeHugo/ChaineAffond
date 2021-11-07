aaa7b126d704070282b6c68b67e8d67a4cddf438
Spourix <bdeseveaux@gmail.com>
2021-11-07 23:05:58 +0100

    corrections logo+fichiers

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express ();
 
 
-app.use(express.static('static'));
+app.use(express.static('/home/deseveaux/Documents/LINFO1212/Projet/ChaineAffond'));
 app.listen(8080);
