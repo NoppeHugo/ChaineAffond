ab5ce7ada66848e64a027764ab6e85dc72480ed7
Spourix <bdeseveaux@gmail.com>
2021-12-20 13:46:30 +0100

    base qui fonctionnait js ready

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var express = require('express');
-var app = express ();
+var consolidate = require('consolidate');
+var app = express();
 
+app.engine ( 'html', consolidate.hogan )
+app.set('views', 'private');
 
+app.get('/', function(req, res){
+    res.render("../static/home.html");
+});
 app.use(express.static('static'));
 app.listen(8080);
