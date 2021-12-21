3efde75eb3c7f366b22618f186ba7fa927977cbd
Spourix <bdeseveaux@gmail.com>
2021-12-21 03:10:26 +0100

    database affichée dans index, tout ok

--- js/main.js
+++ js/main.js
@@ -17,17 +17,30 @@ app.use('/img',express.static(path.resolve(__dirname,"../static/img")));
  MongoClient.connect('mongodb://localhost:27017', (err, db) => {
 	dbo = db.db("data_torpille");
     if (err) throw err;
-    app.get('/index', (req, res) => {
-        dbo.collection('index').findOne({Receveur:"tom"},(err, doc) => {
-            if (err) throw err;
-            res.render('index.html',doc);
-            });
+    app.get('/index.html', (req, res) => {
+        dbo.collection('index').find({}).toArray(function(err,doc) {
+         if (err) throw err;
+        res.render('index.html',
+        {
+           "repo":doc
+        }
+        );
+      });      
     });
     app.get('/',function(req,res){
         res.render("home.html");
      });
      app.get('/home.html',function(req,res){
         res.render("home.html");
+     });
+     app.get('/new.html',function(req,res){
+        res.render("new.html");
+     });
+     app.get('/login.html',function(req,res){
+        res.render("login.html");
+     });
+     app.get('/register.html',function(req,res){
+        res.render("register.html");
      });
      app.get('*', (req, res) => {
         res.status(404).send('Page Not Found voila');
