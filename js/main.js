310ae1a737232ab84efeb5946b62c78224985035
Spourix <bdeseveaux@gmail.com>
2021-12-22 07:54:56 +0100

    la connection fonctionne mais pas l'ajout d'utilisateur

--- js/main.js
+++ js/main.js
@@ -10,13 +10,11 @@ const req = require('express/lib/request');
     https = require('https');
     fs = require('fs');
 
-
 app.engine('html', engines.hogan);
 app.set('view engine','html');
 app.use('/css',express.static(path.resolve(__dirname,"../static/css")));
 app.use('/img',express.static(path.resolve(__dirname,"../static/img")));
 
-/*logged funct*/
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(session({
   secret: "propre123",
@@ -28,87 +26,74 @@ app.use(session({
     maxAge: 3600000
   }
 }));
-/*public zone*/
-app.get('/',function(req,res){
-   res.render("home.html");
-});
-app.get('/home.html',function(req,res){
-   res.render("home.html");
-});
-app.get('/login.html',function(req,res){
-   res.render("login.html");
-});
+
 MongoClient.connect('mongodb://localhost:27017', (err, db) => {
+
    const dba = db.db("sc").collection('accounts');
-   if (err) throw err;
-   app.post('/log',function(req,res, next){
-      dba.findOne({username:req.body.username},(err,doc)=>{
-         if (err) throw err;
-         var a = doc["password"];
-         var b = req.body.password;
-         console.log(a==b);
-         if( a==b ){
-            req.session.username = req.body.username;
-            console.log("log ok");
-            res.render("home.html");
-         }; 
-      console.log("skip"); 
-      });
-      res.redirect("register.html");
-   });
-});
-app.get('/register.html',function(req,res){
-   res.render("register.html");
-});
-
-/*test if acces*/
-function is_log(req, res){
-   if(!req.session.username){
-      res.render("login.html");
-   };
-};
-
-MongoClient.connect('mongodb://localhost:27017', (err, db) => {
    const dbo = db.db("data_torpille").collection('index');
    if (err) throw err;
-   app.get('/index.html', (req, res) => {
-      is_log(req,res);
-      dbo.find({}).toArray(function(err,doc) {
-         if (err) throw err;
-      res.render('index.html',
-         {
-            "repo":doc
-         });
-      });      
+
+   app.get('/',function(req,res){
+      res.render("home.html");
    });
-   /*Ajouter/Envoyer une torpille */
-   app.get('/new.html',(req,res) =>{
-      is_log(req,res);
-      dbo.estimatedDocumentCount().then(function(size){
-         dbo.find().skip(size-1).toArray(function(err,doc){
-            res.render('new.html',doc[0]);    
-         });
+   app.get('/home.html',function(req,res){
+      res.render("home.html");
+   });
+
+   app.get('/login.html',function(req,res){
+      res.render("login.html");
+   });
+   app.post('/log',function(req,res, next){
+      dba.findOne({"username":req.body.username}, (err,doc)=>{
+         if (req.body.password == doc["password"] ) {
+            req.session.username = req.body.username;
+            res.redirect('home.html');
+         }
+         else
+            res.redirect('register.html');
       });
+
+   });
+
+   app.get('/register.html',function(req,res){
+      res.render("register.html");
+   });
+
+   app.get('/index.html', (req, res) => {
+      if(req.session.username){
+         dbo.find({}).toArray(function(err,doc) {
+            res.render('index.html', { "repo":doc } );
+         });
+      }else{
+         res.redirect("/login.html");
+      };      
+   });
+
+   app.get('/new.html',(req,res) =>{
+      if(req.session.username){
+         dbo.estimatedDocumentCount().then(function(size){
+            dbo.find().skip(size-1).toArray(function(err,doc){
+               res.render('new.html',doc[0]);    
+            });
+         });
+      }else{
+         res.redirect("/login.html");
+      };
    });
    app.get('/send', function(req,res) {
-      is_log(req,res);
       if (req.query.InputBoxEmetteur=="" ||req.query.InputBoxRecepteur=="" || req.query.InputDate=="" ){
          res.render('new.html', {succes : "Erreur : Remplissez tous les données correctement"} );
-      }
+      };
       dbo.estimatedDocumentCount().then(function(size){
-         dbo.insertOne({ "date" : req.query.InputDate, "Envoyeur" : req.query.InputBoxEmetteur, "Receveur" : req.query.InputBoxRecepteur, "commentaire" : size+1 })
+         dbo.insertOne({ "date" : req.query.InputDate, "Envoyeur" : req.query.InputBoxEmetteur, "Receveur" : req.query.InputBoxRecepteur, "commentaire" : size+1 });
       });
       res.render('new.html', {succes : "successfully send"} );
    });
+
    app.get('*', (req, res) => {
       res.status(404).send('Page Not Found voila');
    }); 
 });
-
-
-
-
-
 
 https.createServer(
    {
